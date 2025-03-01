86e9f2f67ca3ed8a82ea9e6e2a3b9b815ff8a5a6
Azhaghiri <ganadhan6528@gmail.com>
2025-03-01 07:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-//your JS code here. If required.
+ document.getElementById('btn').addEventListener('click', () => {
+            
+       
+        let ip = document.getElementById('ip').value;
+
+    function resolveNumber() {
+         return new Promise((resolve) => {
+            setTimeout(() => {
+            const number = ip;
+            document.getElementById('output').innerText = "result: "+ number;
+            resolve(number);
+        },2000);
+    });
+    }
+
+        resolveNumber()
+        .then((number) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const multipleNum = number * 2;
+                    document.getElementById('output').innerText = "result: "+ multipleNum;
+                    resolve(multipleNum);
+                }, 3000);
+            });
+        })
+        .then((multipleNum) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const subNum = multipleNum - 3;
+                    document.getElementById("output").innerText = "result: "+ subNum;
+                    resolve(subNum);
+                }, 4000);
+            });
+        })
+        .then((subNum) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const divideNum = subNum / 2;
+                    document.getElementById("output").innerText = "result: "+ divideNum;
+                    resolve(divideNum);
+                }, 5000);
+            });
+        })
+        .then((divideNum) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const addNum = divideNum + 10;
+                    document.getElementById("output").innerText = "result: "+ addNum;
+                    resolve(addNum);
+                }, 6000);
+            })
+        })
+
+    
+
+    });
